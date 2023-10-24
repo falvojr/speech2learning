e9ea959db7bb5f0eda51b230e98dd936f97eacb6
Adriano Carvalho <59294941+driicarvalho7@users.noreply.github.com>
2023-10-24 13:26:55 +0000

    alterações lib.js

--- 6-speech2learning-instance-libras/1-poc-accessible-video-player/src/js/lib.js
+++ 6-speech2learning-instance-libras/1-poc-accessible-video-player/src/js/lib.js
@@ -1,30 +1,54 @@
+// Url inicial para requisições de API
+const url_base = 'https://falvojr.github.io/speech2learning';
+
+// Variável global para manipular retorno da API
+let api_return = {
+    id: '',
+    language: '',
+    metadata: {
+        availableLanguages: {},
+        subtitles: {},
+        transcripts: {}
+    },
+    name: '',
+    url: ''
+}
+
+async function loadVideo() {
+    const sourceElement = document.createElement('source');
+    sourceElement.src = api_return.url;
+    sourceElement.type = 'video/mp4';
+    videoElement.appendChild(sourceElement);
+}
+
+async function loadSubtitles(){
+    api_return.metadata.subtitles.forEach(subtitle => {
+        const trackElement = document.createElement('track');
+        trackElement.kind = 'subtitles';
+        trackElement.label = subtitle.language;
+        trackElement.srclang = subtitle.language;
+        trackElement.src = subtitle.url;
+        trackElement.default = subtitle.language === api_return.language;
+
+        videoElement.appendChild(trackElement);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     const videoElement = document.getElementById('video');
 
     try {
-        const response = await fetch('https://falvojr.github.io/speech2learning/api/mock.json');
+        const response = await fetch(url_base + '/api/mock.json');
         if (!response.ok) throw new Error('Network response was not ok ' + response.statusText);
 
-        const data = await response.json();
+        api_return = await response.json();
 
         // Carregar o vídeo
-        const sourceElement = document.createElement('source');
-        sourceElement.src = data.url;
-        sourceElement.type = 'video/mp4';
-        videoElement.appendChild(sourceElement);
+        await loadVideo();
 
         // Carregar as legendas
-        data.metadata.subtitles.forEach(subtitle => {
-            const trackElement = document.createElement('track');
-            trackElement.kind = 'subtitles';
-            trackElement.label = subtitle.language;
-            trackElement.srclang = subtitle.language;
-            trackElement.src = subtitle.url;
-            trackElement.default = subtitle.language === data.language;
-
-            videoElement.appendChild(trackElement);
-        });
-
+        await loadSubtitles();
+        
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
     }
